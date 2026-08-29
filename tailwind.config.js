c40f99de2ad2b95136c86738f20bfa27767b0b02
VashundharaAsthana <243084468+VashundharaAsthana@users.noreply.github.com>
2026-08-29 17:49:05 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        mullish: ["Mulish", "sans-serif"],
+      },
+       colors: {
+        deepBlue: "#02042a",
+        lightBlue: "#2b84ea",
+        lightBlue300: "#4b94d9",
+        lightBlue500: "#0b72e7",
+        greenLight: "#49DAB5",
+        grayText: "#818597",
+        lightGray: "#e2e2e2",
+        featureBlue:"#181C2E",
+        deepBlueHead: "#1f2556",
+        gray2: "#525a76",
+      },},
+  },
+  plugins: [],
+}
+
